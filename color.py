0e2b07094b89a59ae9ae8575f85eb0c223e0c909
Zet <zetpc235@gmail.com>
2016-01-27 14:04:56 +0000

    shellcode

---
+++ color.py
@@ -0,0 +1,21 @@
+class bcolors:
+    black = '\033[30m'
+    red = '\033[31m'
+    green = '\033[32m'
+    yellow = '\033[33m'
+    blue = '\033[34m'
+    magenta = '\033[35m'
+    cyan = '\033[36m'
+    white = '\033[37m'
+    end = '\033[0m'
+
+    def disable(self):
+        self.black = ''
+        self.red = ''
+        self.green = ''
+        self.yellow = ''
+        self.blue = ''
+        self.magenta = ''
+        self.cyan = ''
+        self.white = ''
+        self.end = ''
