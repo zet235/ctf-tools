483c7e0c9864684ce5de1ec9ff3a7e8bda4acd29
Zet <zetpc235@gmail.com>
2017-02-04 01:31:20 +0800

    add one-gadget-rce and flat function

--- pylib/zp/pack.py
+++ pylib/zp/pack.py
@@ -11,3 +11,23 @@ def p64(data,fmt="<Q"):
 
 def u64(data,fmt="<Q"):
     return struct.unpack(fmt,data)[0]
+
+def flat32(*args, **kwarg):
+    out = []
+    for arg in args:
+        if type(arg) == list:
+            for i in range(len(arg)):
+                out.append(arg[i])
+        else :
+            out.append(arg)
+    return ''.join(map(p32,out))
+
+def flat64(*args, **kwarg):
+    out = []
+    for arg in args:
+        if type(arg) == list:
+            for i in range(len(arg)):
+                out.append(arg[i])
+        else :
+            out.append(arg)
+    return ''.join(map(p64,out))
