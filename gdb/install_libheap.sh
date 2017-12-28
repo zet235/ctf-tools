2f1db22e17e0b16b4e740733cea166850cc8ad6f
Zet <f4ke.tw@gmail.com>
2017-12-28 16:05:05 +0800

    add libheap

---
+++ gdb/install_libheap.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+pip3 install --user ./libheap/
+#echo "python import sys" >> ~/.gdbinit
+#echo "python sys.path.append('~/.local/lib/python3.4/site-packages/')" >> ~/.gdbinit
+#echo "python from libheap import *" >> ~/.gdbinit
